75a421e2e445ad154ad81fa27c883adddb2f7a1b
IvanGodPro24 <vanyanep@gmail.com>
2025-02-26 22:47:58 +0200

    Add model +  database interaction

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import cors from 'cors';
 // const PORT = Number(process.env.PORT);
 
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllStudents, getStudentById } from './services/students.js';
+import mongoose from 'mongoose';
 
 const PORT = Number(getEnvVar('PORT', '8080'));
 
@@ -27,6 +29,39 @@ export const startServer = () => {
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
+  });
+
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
+    });
+  });
+
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(studentId)) {
+      return res.status(400).json({ message: 'Invalid student ID Format' });
+    }
+
+    try {
+      const student = await getStudentById(studentId);
+
+      if (!student) {
+        res.status(404).json({
+          message: 'Student not found!',
+        });
+        return;
+      }
+
+      res.status(200).json({
+        data: student,
+      });
+    } catch (error) {
+      next(error);
+    }
   });
 
   app.use('*', (req, res, next) => {

334441038cc6e9b465a85aa9184fd035340bd0c9
IvanGodPro24 <vanyanep@gmail.com>
2025-03-03 17:59:30 +0200

    Add controllers

---
+++ src/controllers/students.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import { getAllStudents, getStudentById } from '../services/students.js';
+
+export const getStudentsController = async (req, res) => {
+  const students = await getAllStudents();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found students!',
+    data: students,
+  });
+};
+
+export const getStudentByIdController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(studentId)) {
+    return res.status(400).json({ message: 'Invalid student ID Format' });
+  }
+
+  try {
+    const student = await getStudentById(studentId);
+
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found!',
+      });
+      return;
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found student with id ${studentId}!`,
+      data: student,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/students.js
+++ src/routes/students.js
@@ -1,40 +1,13 @@
 import { Router } from 'express';
-import mongoose from 'mongoose';
-import { getAllStudents, getStudentById } from '../services/students.js';
+import {
+  getStudentByIdController,
+  getStudentsController,
+} from '../controllers/students.js';
 
 const router = Router();
 
-router.get('/students', async (req, res) => {
-  const students = await getAllStudents();
+router.get('/students', getStudentsController);
 
-  res.status(200).json({
-    data: students,
-  });
-});
-
-router.get('/students/:studentId', async (req, res, next) => {
-  const { studentId } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(studentId)) {
-    return res.status(400).json({ message: 'Invalid student ID Format' });
-  }
-
-  try {
-    const student = await getStudentById(studentId);
-
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found!',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      data: student,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/students/:studentId', getStudentByIdController);
 
 export default router;

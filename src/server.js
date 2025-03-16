050e5518c0129775aa211fe1d5c948649cf38db3
IvanGodPro24 <vanyanep@gmail.com>
2025-03-16 22:43:32 +0200

    Add cookies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 // import dotenv from 'dotenv';
 
 // dotenv.config();
@@ -20,6 +21,7 @@ export const startServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
